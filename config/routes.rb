326eccdcb63ae9f95abd13cc26021218e735bba8
Jose Daniel <jodorganistaca@unal.edu.co>
2021-04-05 23:20:11 -0500

    change name of one route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :toll_locators
+  post "findToll", to: "toll_locators#create"
   resources :tolls
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
